aa5eebe4e164baf7c228a83e247169af2f8d1250
Maria Waldl <maria@tbi.univie.ac.at>
2024-02-15 18:14:20 +0100

    add initial setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("src/RRIkinDP/__init__.py") as f:
+    for line in f.readlines():
+        if line.startswith('__version__'):
+            VERSION = line.strip().split()[-1][1:-1]
+
+#with open('requirements.txt') as f:
+#    requirements = f.read().splitlines()
+
+setup(
+    name='RRIkinDP',
+    version=VERSION,
+    packages=find_packages('src'),
+    package_dir={'':'src'},
+    description='Evaluation of thermodynamic and kinetic features of RNA-RNA interactions',
+    author='Maria Waldl',
+    author_email='code@waldl.org',
+    url='https://github.com/mwaldl/RRIkinDP',
+#    install_requires=requirements,
+    python_requires='>=3.6'
+)
