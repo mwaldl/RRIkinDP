a368b40d074e34c50e47f80c3bf1668fb915b126
Maria Waldl <maria@tbi.univie.ac.at>
2024-02-15 11:13:54 +0100

    ad init.py for the python analysis scripts

---
+++ src/RRIkinDP/__init__.py
@@ -0,0 +1,26 @@
+ 
+#
+# RRIkinDP -
+# (C) Maria Waldl, 2023
+#
+# This file is part of the RRIkinDP source code.
+#
+# RRIkinDP generates the states on direct paths of RNA-RNA interactions formation and
+# and computes barrier state energies on direct RNA-RNA interaction formation paths.
+#
+
+## @file __init__.py
+# Package RRIkinDP
+
+""" @package RRIkinDP
+Package RRIkinDP
+
+Package defining aditional analysis scripts for RRIkinDP.
+
+"""
+
+from . import landscape
+from . import utilities
+
+
+__version__ = '0.0.1'
