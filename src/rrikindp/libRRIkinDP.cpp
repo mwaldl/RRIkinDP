5fce13e71a6f44524f3a634ed8ff5fbf484fb265
Maria Waldl <maria@tbi.univie.ac.at>
2025-02-12 20:48:41 +0100

    add BasePair class into pybind11 bindings to work with IntaRNA 3.4.1

--- src/rrikindp/libRRIkinDP.cpp
+++ src/rrikindp/libRRIkinDP.cpp
@@ -18,7 +18,7 @@ PYBIND11_MODULE(libRRIkinDP, m) {
     // Bind the EM (energy matrix) class
     py::class_<EM>(m, "EM")
         //.def(py::init<IntaRNA::Interaction, std::string, std::string,
-        //std::string, std::string, bool, bool, double>())
+        // std::string, std::string, bool, bool, double>())
         .def(py::init<const IntaRNA::Interaction, const std::string &,
                       const std::string &, const std::string &,
                       const std::string &, bool, bool, double>())
@@ -28,15 +28,21 @@ PYBIND11_MODULE(libRRIkinDP, m) {
         .def("get_ED1", &EM::get_ED1)
         .def("get_ED2", &EM::get_ED2)
         .def("get_minBarrier", &EM::get_minBarrier);
-    // Bind IntaRNA RnaSequence
-    py::class_<IntaRNA::RnaSequence>(
-        m, "RnaSequence") // IntaRNA::RnaSequence s2(seq_b_id, seq_b);
+    // Bind IntaRNA RnaSequence class
+    // IntaRNA::RnaSequence s2(seq_b_id, seq_b);
+    py::class_<IntaRNA::RnaSequence>(m, "RnaSequence")
         .def(py::init<const std::string, const std::string>());
-    // Bind the Intarna Interaction class  // IntaRNA::Interaction
-    // interaction(s1, s2); interaction.basePairs = bps_list_b0;
+    // Bind the Intarna Interaction class
+    // IntaRNA::Interaction interaction(s1, s2);
+    // interaction.basePairs = bps_list_b0;
     py::class_<IntaRNA::Interaction>(m, "Interaction")
         .def(py::init<const IntaRNA::RnaSequence, const IntaRNA::RnaSequence>())
         .def_readwrite("basePairs", &IntaRNA::Interaction::basePairs)
         .def("is_Valid", &IntaRNA::Interaction::isValid)
         .def("is_Empty", &IntaRNA::Interaction::isEmpty);
+    // Bind IntaRNA IntaRNA Interaction::BasePair class
+    py::class_<IntaRNA::Interaction::BasePair>(m, "BasePair")
+        .def(py::init<const int, const int>())
+        .def_readwrite("first", &IntaRNA::Interaction::BasePair::first)
+        .def_readwrite("second", &IntaRNA::Interaction::BasePair::second);
 }
