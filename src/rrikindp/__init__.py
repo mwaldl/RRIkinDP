41ed1443d49b1309361c48ac7406ab5f6dc3dd59
Maria Waldl <maria@tbi.univie.ac.at>
2025-02-12 20:49:44 +0100

    update python init file to relflect new C++ bindings as well as renaming the MarkovProcess and DPLandscape class

--- src/rrikindp/__init__.py
+++ src/rrikindp/__init__.py
@@ -4,15 +4,14 @@
 #
 # This file is part of the RRIkinDP source code.
 #
-# RRIkinDP generates the states on direct paths of RNA-RNA interactions formation and
+# RRIkinDP generates the states on direct paths of RNA-RNA interactions formation
 # and computes barrier state energies on direct RNA-RNA interaction formation paths.
 # The rrikindp python modules provides additional analysis and plotting scripts.
 #
 
-
-from libRRIkinDP import EM, RnaSequence, Interaction
 from . import utilities
-from landscape import DPLandscape, plot_landscape
-from masterequation import States, MP
+from .landscape import DPLandscape, plot_landscape
+from .masterequation import State, MarkovProcess
+from libRRIkinDP import EM, RnaSequence, Interaction, BasePair
 
 __version__ = "v0.0.3"
